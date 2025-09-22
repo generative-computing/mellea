2f27026e037a07b8162b2e57925b8eae609b8f14
jakelorocco <jake.lorocco@ibm.com>
2025-09-21 22:08:29 -0400

    fix: watsonx tests

--- test/backends/test_watsonx.py
+++ test/backends/test_watsonx.py
@@ -36,70 +36,70 @@ def session(backend: WatsonxAIBackend):
         session.reset()
 
 
-# @pytest.mark.qualitative
-# def test_instruct(session: MelleaSession):
-#     result = session.instruct("Compute 1+1.")
-#     assert isinstance(result, ModelOutputThunk)
-#     assert "2" in result.value  # type: ignore
+@pytest.mark.qualitative
+def test_instruct(session: MelleaSession):
+    result = session.instruct("Compute 1+1.")
+    assert isinstance(result, ModelOutputThunk)
+    assert "2" in result.value  # type: ignore
+
+@pytest.mark.xfail(reason="watsonx python sdk has weird interactions with event loops; causes some errors with pytest.")
+@pytest.mark.qualitative
+def test_multiturn(session: MelleaSession):
+    session.instruct("What is the capital of France?")
+    answer = session.instruct("Tell me the answer to the previous question.")
+    assert "Paris" in answer.value  # type: ignore
+
+@pytest.mark.qualitative
+def test_chat(session):
+    output_message = session.chat("What is 1+1?")
+    assert "2" in output_message.content, (
+        f"Expected a message with content containing 2 but found {output_message}"
+    )
+
+@pytest.mark.xfail(reason="watsonx python sdk has weird interactions with event loops; causes some errors with pytest.")
+@pytest.mark.qualitative
+def test_format(session: MelleaSession):
+    class Person(pydantic.BaseModel):
+        name: str
+        # it does not support regex patterns in json schema
+        email_address: str
+        # email_address: Annotated[
+        #     str,
+        #     pydantic.StringConstraints(pattern=r"[a-zA-Z]{5,10}@example\.com"),
+        # ]
+
+    class Email(pydantic.BaseModel):
+        to: Person
+        subject: str
+        body: str
+
+    output = session.instruct(
+        "Write a short email to Olivia, thanking her for organizing a sailing activity. Her email server is example.com. No more than two sentences. ",
+        format=Email,
+        model_options={ModelOption.MAX_NEW_TOKENS: 2**8},
+    )
+    print("Formatted output:")
+    email = Email.model_validate_json(
+        output.value
+    )  # this should succeed because the output should be JSON because we passed in a format= argument...
+    print(email)
+
+    print("address:", email.to.email_address)
+    # this is not guaranteed, due to the lack of regexp pattern
+    # assert "@" in email.to.email_address
+    # assert email.to.email_address.endswith("example.com")
+    pass
 
 
-# @pytest.mark.qualitative
-# def test_multiturn(session: MelleaSession):
-#     session.instruct("What is the capital of France?")
-#     answer = session.instruct("Tell me the answer to the previous question.")
-#     assert "Paris" in answer.value  # type: ignore
+@pytest.mark.qualitative
+def test_generate_from_raw(session: MelleaSession):
+    prompts = ["what is 1+1?", "what is 2+2?", "what is 3+3?", "what is 4+4?"]
 
-# @pytest.mark.qualitative
-# def test_chat(session):
-#     output_message = session.chat("What is 1+1?")
-#     assert "2" in output_message.content, (
-#         f"Expected a message with content containing 2 but found {output_message}"
-#     )
+    results = session.backend._generate_from_raw(
+        actions=[CBlock(value=prompt) for prompt in prompts], generate_logs=None
+    )
 
-
-# @pytest.mark.qualitative
-# def test_format(session: MelleaSession):
-#     class Person(pydantic.BaseModel):
-#         name: str
-#         # it does not support regex patterns in json schema
-#         email_address: str
-#         # email_address: Annotated[
-#         #     str,
-#         #     pydantic.StringConstraints(pattern=r"[a-zA-Z]{5,10}@example\.com"),
-#         # ]
-
-#     class Email(pydantic.BaseModel):
-#         to: Person
-#         subject: str
-#         body: str
-
-#     output = session.instruct(
-#         "Write a short email to Olivia, thanking her for organizing a sailing activity. Her email server is example.com. No more than two sentences. ",
-#         format=Email,
-#         model_options={ModelOption.MAX_NEW_TOKENS: 2**8},
-#     )
-#     print("Formatted output:")
-#     email = Email.model_validate_json(
-#         output.value
-#     )  # this should succeed because the output should be JSON because we passed in a format= argument...
-#     print(email)
-
-#     print("address:", email.to.email_address)
-#     # this is not guaranteed, due to the lack of regexp pattern
-#     # assert "@" in email.to.email_address
-#     # assert email.to.email_address.endswith("example.com")
-#     pass
-
-
-# @pytest.mark.qualitative
-# def test_generate_from_raw(session: MelleaSession):
-#     prompts = ["what is 1+1?", "what is 2+2?", "what is 3+3?", "what is 4+4?"]
-
-#     results = session.backend._generate_from_raw(
-#         actions=[CBlock(value=prompt) for prompt in prompts], generate_logs=None
-#     )
-
-#     assert len(results) == len(prompts)
+    assert len(results) == len(prompts)
 
 def test_async_parallel_requests(session):
     async def parallel_requests():
