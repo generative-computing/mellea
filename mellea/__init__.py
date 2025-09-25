c9ec7df2a8a2be97df7b3dd53ce608ab2d7426ef
Hendrik Strobelt <hendrik.strobelt@ibm.com>
2025-09-25 18:01:58 -0400

    maybe breaking:
    removing chat, instruct,.. from mellea/__init__.py

--- mellea/__init__.py
+++ mellea/__init__.py
@@ -3,26 +3,13 @@
 import mellea.backends.model_ids as model_ids
 from mellea.stdlib.base import LegacyLinearContext, LegacySimpleContext
 from mellea.stdlib.genslot import generative
-from mellea.stdlib.session import (
-    MelleaSession,
-    chat,
-    instruct,
-    query,
-    start_session,
-    transform,
-    validate,
-)
+from mellea.stdlib.session import MelleaSession, start_session
 
 __all__ = [
     "LegacyLinearContext",
     "LegacySimpleContext",
     "MelleaSession",
-    "chat",
     "generative",
-    "instruct",
     "model_ids",
-    "query",
     "start_session",
-    "transform",
-    "validate",
 ]
