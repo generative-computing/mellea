5b11c5cb0b2983a29e1e528e331f69409640e225
Hendrik Strobelt <hendrik.strobelt@ibm.com>
2025-09-26 17:17:04 -0400

    more examples updated

--- docs/examples/tutorial/context_example.py
+++ docs/examples/tutorial/context_example.py
@@ -1,9 +1,10 @@
-from mellea import LegacyLinearContext, start_session
+from mellea import start_session
+from mellea.stdlib.base import ChatContext
 
-m = start_session(ctx=LegacyLinearContext())
+m = start_session(ctx=ChatContext())
 m.chat("Make up a math problem.")
 m.chat("Solve your math problem.")
 
 print(m.ctx.last_output())
-
+print("==================")
 print(m.ctx.last_turn())
