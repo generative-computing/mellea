ae578312f53749fe24207317fd3d52a0ac539f8c
jakelorocco <jake.lorocco@ibm.com>
2025-09-30 13:34:15 -0400

    test: add minimum test examples for async session funcs

--- test/stdlib_basics/test_session.py
+++ test/stdlib_basics/test_session.py
@@ -2,9 +2,27 @@ import os
 
 import pytest
 
+from mellea.backends.types import ModelOption
 from mellea.stdlib.base import ModelOutputThunk
+from mellea.stdlib.chat import Message
 from mellea.stdlib.session import start_session
 
+@pytest.fixture(scope="module")
+def m_session(gh_run):
+    if gh_run == 1:
+        m = start_session(
+            "ollama",
+            model_id="llama3.2:1b",
+            model_options={ModelOption.MAX_NEW_TOKENS: 5},
+        )
+    else:
+        m = start_session(
+            "ollama",
+            model_id="granite3.3:8b",
+            model_options={ModelOption.MAX_NEW_TOKENS: 5},
+        )
+    yield m
+    del m
 
 def test_start_session_watsonx(gh_run):
     if gh_run == 1:
@@ -14,7 +32,6 @@ def test_start_session_watsonx(gh_run):
         response = m.instruct("testing")
         assert isinstance(response, ModelOutputThunk)
         assert response.value is not None
-
 
 def test_start_session_openai_with_kwargs(gh_run):
     if gh_run == 1:
@@ -37,6 +54,17 @@ def test_start_session_openai_with_kwargs(gh_run):
     assert response.value is not None
     assert initial_ctx is not m.ctx
 
+async def test_aact(m_session):
+    initial_ctx = m_session.ctx
+    out = await m_session.aact(Message(role="user", content="Hello!"))
+    assert m_session.ctx is not initial_ctx
+    assert out.value is not None
+
+async def test_ainstruct(m_session):
+    initial_ctx = m_session.ctx
+    out = await m_session.ainstruct("Write a sentence.")
+    assert m_session.ctx is not initial_ctx
+    assert out.value is not None
 
 if __name__ == "__main__":
     pytest.main([__file__])
