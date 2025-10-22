fe8d181dd496e85ff5fac24925d6bbd137cba8ee
supriyalall <lallsupriya365@gmail.com>
2025-10-21 17:27:24 -0700

    adding requirements

--- docs/examples/bee_agent.py
+++ docs/examples/bee_agent.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from typing import Annotated
 
-from a2a.types import Message
+from mellea.stdlib.chat import Message
 from a2a.utils.message import get_message_text
 from beeai_sdk.server import Server
 from beeai_sdk.a2a.types import AgentMessage
@@ -19,63 +19,48 @@ from mellea.backends.openai import OpenAIBackend
 from mellea.stdlib.requirement import req, Requirement, simple_validate
 from mellea.stdlib.sampling import RejectionSamplingStrategy
 from mellea.stdlib.base import ChatContext
+from beeai_framework.backend.chat import ChatModel
+from beeai_framework.adapters.beeai_platform.serve.server import BeeAIPlatformMemoryManager, BeeAIPlatformServer
+
 
 server = Server()
 
 from typing import Callable, Tuple
 
-#form_render = FormRender(
-#    id="party_invite_form",
-#    title="Party Invitation",
-#    description="Enter the person's name to generate an invitation",
-#    columns=1,
-#    submit_label="Generate",
-#    fields=[
-#        TextField(
-#            type="text",
-#            id="name",
-#            label="Name",
-#            placeholder="Enter the person's name",
-#            required=True,
-#            col_span=1
-#        )
-#    ]
-#)
 
-def generate_response(m: MelleaSession, prompt: str):
-    return m.chat(prompt)
 
-def bee_app(func: Callable, **inputs):
-    inputs = inputs.get("inputs", {})
-    @server.agent(name="mellea_agent", detail=AgentDetail(interaction_mode="single-turn"))
-    async def wrapper(llm: Annotated[LLMServiceExtensionServer, LLMServiceExtensionSpec.single_demand()], 
+
+def generate_response(m: MelleaSession, prompt: str, requirements: list[Requirement]):
+    # Logs user messages to the context and passes prompt through m object
+    user_msg = Message(role="user", content=prompt)
+    m.ctx.add(user_msg) #Adds the user message to the context (Message objects are not JSON serializable)
+    return m.instruct(prompt, requirements=requirements)
+
+def bee_app(func: Callable, **inputs) -> Callable:
+     @server.agent(name="mellea_agent", detail=AgentDetail(interaction_mode="multi-turn"))
+
+     async def wrapper(llm: Annotated[LLMServiceExtensionServer, LLMServiceExtensionSpec.single_demand()], 
                      trajectory: Annotated[TrajectoryExtensionServer, TrajectoryExtensionSpec()],
 		     form: Annotated[FormExtensionServer, FormExtensionSpec(params=None)]):
-      	
-        name = "Alison"
-        age = "28" 
-
+        
+        name = "angela"
+        age = "28"
         llm_config = llm.data.llm_fulfillments.get("default")
-    
-        m = MelleaSession(OpenAIBackend(
+        model = ChatModel.from_name("ollama:llama3.1")
+       	m = MelleaSession(OpenAIBackend(
             model_id=llm_config.api_model,
             api_key=llm_config.api_key,
             base_url=llm_config.api_base
         ))
-
-	#requirements = [req("Be formal."), req("Be funny."), Requirement("Use less than 100 words.", validation_fn=simple_validate(lambda o: len(o.split()) < 100))]
- 
+        requirements = [req("Be formal."), req("Be funny."), Requirement("Use less than 100 words.", validation_fn=simple_validate(lambda o: len(o.split()) < 100))]
         prompt, callable = func(name, age) 
-        result = await asyncio.to_thread(
-            callable,
-            m,
-            prompt,
-        )
+        result = await asyncio.to_thread(callable, m, prompt, requirements)
         yield AgentMessage(text=m.ctx.last_output().value)
-    return wrapper
+     return wrapper
 
 @bee_app
-def new_func(name: str, age: int) -> Tuple[ str, Callable[ [str], str] ]:
+def new_func(name: str, age: int) -> Tuple[ str, Callable ]:
+    # Takes in name and age, and outputs both prompt and callable (m.chat)
     prompt = f"Write a birthday card for {name}. Include their age: {age}"
     return (prompt, generate_response)
 
