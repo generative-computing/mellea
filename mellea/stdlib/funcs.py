0dbe7b86c3ef8479476c2b7bfdc6041deb2c5307
Hendrik Strobelt <hendrik.strobelt@ibm.com>
2025-10-23 19:04:45 -0400

    fixing validate confusions between explicit output and implicit output in context. fixing test_funcs.py

--- mellea/stdlib/funcs.py
+++ mellea/stdlib/funcs.py
@@ -263,9 +263,9 @@ def chat(
 
 def validate(
     reqs: Requirement | list[Requirement],
-    context: Context,
     backend: Backend,
     *,
+    context: Context | None = None,
     output: CBlock | None = None,
     format: type[BaseModelSubclass] | None = None,
     model_options: dict | None = None,
@@ -701,9 +701,9 @@ async def achat(
 
 async def avalidate(
     reqs: Requirement | list[Requirement],
-    context: Context,
     backend: Backend,
     *,
+    context: Context | None = None,
     output: CBlock | None = None,
     format: type[BaseModelSubclass] | None = None,
     model_options: dict | None = None,
@@ -720,6 +720,7 @@ async def avalidate(
     )
 
     if output is None:
+        assert context is not None
         validation_target_ctx = context
     else:
         validation_target_ctx = SimpleContext()
