e2e9141a98a96499f8f6c1157e8700008dea1dfe
supriyalall <lallsupriya365@gmail.com>
2025-10-27 21:26:27 -0400

    working example of decorator

--- docs/examples/bee_agent.py
+++ docs/examples/bee_agent.py
@@ -21,26 +21,25 @@ from mellea.stdlib.sampling import RejectionSamplingStrategy
 from mellea.stdlib.base import ChatContext
 from beeai_framework.backend.chat import ChatModel
 from beeai_framework.adapters.beeai_platform.serve.server import BeeAIPlatformMemoryManager, BeeAIPlatformServer
-from mellea.bee_platform import bee_app
+from bee_platform import bee_app
 
 server = Server()
 
 from typing import Callable, Tuple
 
 
-
-
 @bee_app
-def new_func(name: str, age: int) -> Tuple[ str, Callable ]:
-    # Takes in name and age, and outputs both prompt and callable (m.chat)
-    response = m.chat(f"Write a birthday card for {name}, {name} is {age}")
-    return response.value
+def mellea_func(m: MelleaSession, name: str, age: int) -> str:
+    """
+    Example email writing module that starts a Mellea session and adds any calls to an internal ChatContext
+    Inputs:
+        name: str
+        age: str
+    """
+    response = m.chat(f"Write an email for {name}, {name} is {age}")
+    return response.content
 
 
 
-def run():
-    server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
 
 
-if __name__ == "__main__":
-    run()
