9ec1020fd9ee3514af614d6beb8500178cb2c2f1
Nathan Fulton <nathan@ibm.com>
2025-11-20 12:10:40 -0500

    Adds \n and removes unused import in base.py

--- mellea/stdlib/base.py
+++ mellea/stdlib/base.py
@@ -16,6 +16,8 @@ from typing import Any, Protocol, TypeVar, runtime_checkable
 
 from PIL import Image as PILImage
 
+from mellea.helpers.fancy_logger import FancyLogger
+
 
 class CBlock:
     """A `CBlock` is a block of content that can serve as input to or output from an LLM."""
