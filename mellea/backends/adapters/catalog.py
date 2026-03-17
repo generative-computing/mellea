9c0b0a96f7fbf222bc11f18fb04118406e9c5b70
inkpad <inkit.padhi@gmail.com>
2026-03-17 14:48:42 +0000

    fix rc repo name

--- mellea/backends/adapters/catalog.py
+++ mellea/backends/adapters/catalog.py
@@ -64,7 +64,7 @@ _INTRINSICS_CATALOG_ENTRIES = [
     ############################################
     # Core Intrinsics
     ############################################
-    IntriniscsCatalogEntry(name="requirement_check", repo_id=_CORE_R1_REPO),
+    IntriniscsCatalogEntry(name="requirement-check", repo_id=_CORE_R1_REPO),
     IntriniscsCatalogEntry(name="uncertainty", repo_id=_CORE_R1_REPO),
     ############################################
     # RAG Intrinsics
