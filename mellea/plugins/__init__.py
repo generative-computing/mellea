1a571c60c8e673dbe540425a5553ff0dbb4b9fff
Frederico Araujo <frederico.araujo@ibm.com>
2026-04-26 09:06:45 -0400

    fix: skip tool hooks for framework-internal tools
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- mellea/plugins/__init__.py
+++ mellea/plugins/__init__.py
@@ -9,6 +9,11 @@ from __future__ import annotations
 
 from .base import Plugin, PluginResult, PluginViolationError
 from .decorators import hook
+from .manager import (
+    is_internal_tool,
+    set_skip_hooks_for_internal_tools,
+    skip_hooks_for_internal_tools,
+)
 from .pluginset import PluginSet
 from .registry import block, modify, plugin_scope, register, unregister
 from .types import HookType, PluginMode
@@ -22,8 +27,11 @@ __all__ = [
     "PluginViolationError",
     "block",
     "hook",
+    "is_internal_tool",
     "modify",
     "plugin_scope",
     "register",
+    "set_skip_hooks_for_internal_tools",
+    "skip_hooks_for_internal_tools",
     "unregister",
 ]
