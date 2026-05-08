fae04d2279c6c67516acfe78cf2fc326eaf4e8a9
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2026-05-08 18:01:19 -0400

    shell executor
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- mellea/stdlib/tools/__init__.py
+++ mellea/stdlib/tools/__init__.py
@@ -1,5 +1,11 @@
 """Implementations of tools."""
 
 from .interpreter import code_interpreter, local_code_interpreter
+from .shell import bash_executor, local_bash_executor
 
-__all__ = ["code_interpreter", "local_code_interpreter"]
+__all__ = [
+    "bash_executor",
+    "code_interpreter",
+    "local_bash_executor",
+    "local_code_interpreter",
+]
