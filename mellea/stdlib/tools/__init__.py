b0d9b7c6a1f65cbdca23d2f02ddb7596ba4a65ad
Akihiko Kuroda <akihikokuroda2020@gmail.com>
2026-05-12 10:48:40 -0400

    review comments
    Signed-off-by: Akihiko Kuroda <akihikokuroda2020@gmail.com>

--- mellea/stdlib/tools/__init__.py
+++ mellea/stdlib/tools/__init__.py
@@ -1,11 +1,20 @@
 """Implementations of tools."""
 
 from .interpreter import code_interpreter, local_code_interpreter
-from .shell import bash_executor, local_bash_executor
+from .shell import (
+    BashEnvironment,
+    LLMSandboxBashEnvironment,
+    StaticBashEnvironment,
+    bash_executor,
+    unsafe_local_bash_executor,
+)
 
 __all__ = [
+    "BashEnvironment",
+    "LLMSandboxBashEnvironment",
+    "StaticBashEnvironment",
     "bash_executor",
     "code_interpreter",
-    "local_bash_executor",
     "local_code_interpreter",
+    "unsafe_local_bash_executor",
 ]
