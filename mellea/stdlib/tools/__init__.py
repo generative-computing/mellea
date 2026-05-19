59b7b88e866e0ad0afa2b2e3bf7fcdd985aa06ac
Akihiko (Aki) Kuroda <16141898+akihikokuroda@users.noreply.github.com>
2026-05-19 14:05:45 -0400

    Revert "feat: add bash tool  (#1056)" (#1098)
    This reverts commit 91d3f59acbcd3651126750d7c7634b9531102313.

--- mellea/stdlib/tools/__init__.py
+++ mellea/stdlib/tools/__init__.py
@@ -1,20 +1,5 @@
 """Implementations of tools."""
 
 from .interpreter import code_interpreter, local_code_interpreter
-from .shell import (
-    BashEnvironment,
-    LLMSandboxBashEnvironment,
-    StaticBashEnvironment,
-    bash_executor,
-    unsafe_local_bash_executor,
-)
 
-__all__ = [
-    "BashEnvironment",
-    "LLMSandboxBashEnvironment",
-    "StaticBashEnvironment",
-    "bash_executor",
-    "code_interpreter",
-    "local_code_interpreter",
-    "unsafe_local_bash_executor",
-]
+__all__ = ["code_interpreter", "local_code_interpreter"]
