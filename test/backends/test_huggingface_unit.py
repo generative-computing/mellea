generative-computing/mellea

commit 6205b36a19b611602d814dc026b7dfbf1b1f6f11

refactor: llguidance (#288)
* fix(conda): conda should not always supersede mamba

* fix(conda): fixed existing conda environment detection; fails if there is any env name with "mellea"

* refactor: outlines -> llguidance on vllm/hf, vllm 0.13, V1 API, disable vllm on osx

* fix: revert BaseModelSubclass default value

* fix: removed unused Generic from ReactInitiator

* fix(docs): pass docs/kv_smash/kvcache.py

* fixup! fix: revert BaseModelSubclass default value

* fixup! refactor: outlines -> llguidance on vllm/hf, vllm 0.13, V1 API, disable vllm on osx

* fixup! refactor: outlines -> llguidance on vllm/hf, vllm 0.13, V1 API, disable vllm on osx

* fixup! refactor: outlines -> llguidance on vllm/hf, vllm 0.13, V1 API, disable vllm on osx

mellea/backends/huggingface.py
@@ -15,12 +15,14 @@ from collections.abc import Callable, Coroutine, Sequence
 from typing import Any, overload
 
 import granite_common
-import outlines
-import outlines_core
+import llguidance
+import llguidance.hf
+import llguidance.torch
 import peft
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.cache_utils import DynamicCache
+from transformers.generation.logits_process import LogitsProcessorList
 from transformers.generation.streamers import AsyncTextIteratorStreamer
 from transformers.generation.utils import GenerateDecoderOnlyOutput
 from transformers.modeling_utils import PreTrainedModel
@@ -67,8 +69,6 @@ from .tools import (
 )
 from .utils import to_chat, to_tool_calls
 
-assert outlines, "outlines needs to be present to make outlines_core work"
-
 """A configuration type for the unhappy path: Tokenizer * Model * torch device string
 
 Huggingface backends can initialize themselves from a model string if the transformers `Auto*` classes can be used. Therefore, a TransformersTorchConfig usually isn't required. However, sometimes a model needs special care to instantiate properly, or a custom device type needs to bse used. Instead of trying to do a lot of partial magic, we basically have two modaliites: either the constructor can figure out everything from the model_id, or the user has to provide an entire config.
@@ -86,6 +86,65 @@ class HFAloraCacheInfo:
     merged_token_ids: Any
     merged_attention: Any
     q_end: int = -1
+
+
+# modified from VLLM v0.9.2 code base
+# https://github.com/vllm-project/vllm/blob/v0.9.2/vllm/model_executor/guided_decoding/guidance_logits_processors.py
+class _GuidanceLogitsProcessor:
+    def __init__(self, grammar: str, ll_tokenizer: llguidance.LLTokenizer) -> None:
+        self.grammar = grammar
+        self.vocab_size: int = ll_tokenizer.vocab_size
+        self.ll_tokenizer: llguidance.LLTokenizer = ll_tokenizer
+        self.ll_matchers: list[llguidance.LLMatcher] = []
+        self.bitmasks: list[torch.Tensor] = []
+        self.new_sampling: bool = False
+        self.batch_size: int = -1
+
+    def __call__(
+        self, batch_input_ids: torch.Tensor, batch_scores: torch.Tensor
+    ) -> torch.Tensor:
+        i_batch, _ = batch_input_ids.shape
+        s_batch, _ = batch_scores.shape
+        assert i_batch == s_batch
+
+        # s_batch, s_vocab = batch_scores.shape
+        # assert s_vocab == self.vocab_size
+        #
+        # NOTE: somehow, this does not hold. s_vocab is not same as either of
+        # * self._tokenizer._tokenizer.get_vocab_size(with_added_tokens=True) == self.vocab_size == ll_tokenizer.vocab_size
+        # * self._tokenizer._tokenizer.get_vocab_size(with_added_tokens=False)
+
+        if self.batch_size != i_batch:
+            self.batch_size = i_batch
+            self.bitmasks = [
+                llguidance.torch.allocate_token_bitmask(1, self.vocab_size)  # type: ignore[attr-defined]
+                for _ in range(self.batch_size)
+            ]
+
+            self.ll_matchers = [
+                llguidance.LLMatcher(self.ll_tokenizer, self.grammar)
+                for _ in range(self.batch_size)
+            ]
+
+        for input_ids, scores, ll_matcher, bitmask in zip(
+            batch_input_ids, batch_scores, self.ll_matchers, self.bitmasks
+        ):
+            if self.new_sampling and len(input_ids) > 0:
+                ll_matcher.consume_token(  # type: ignore[attr-defined]
+                    input_ids.tolist()[-1]
+                )
+                err = ll_matcher.get_error()  # type: ignore[attr-defined]
+                if err:
+                    FancyLogger.get_logger().warning("Error in LLMatcher: %s", err)
+
+            llguidance.torch.fill_next_token_bitmask(ll_matcher, bitmask, 0)
+            llguidance.torch.apply_token_bitmask_inplace(
+                scores, bitmask.to(scores.device)
+            )  # type: ignore[attr-defined]
+
+        self.new_sampling = True
+
+        return batch_scores
 
 
 class LocalHFBackend(FormatterBackend, AdapterMixin):
@@ -175,6 +234,14 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
                 )
             case _:
                 self._tokenizer, self._model, self._device = custom_config
+
+        self._llguidance_tokenizer: llguidance.LLTokenizer = (
+            llguidance.hf.from_tokenizer(self._tokenizer)  # type:ignore
+        )
+        assert (
+            self._llguidance_tokenizer.vocab_size
+            == self._tokenizer._tokenizer.get_vocab_size(with_added_tokens=True)
+        ), "vocab size mismatch between llguidance and huggingface tokenizers ... wtf?"
 
         self._use_caches = use_caches
         self._cache = cache if cache is not None else SimpleLRUCache(3)
@@ -609,24 +676,15 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
 
             format_kwargs = {}
             if _format:
-                # outlines.generate.json always parses the resulting json into a python dict.
-                # We however want to keep it as a json string for later storing it in ModelOutputThunk
-                schema: dict[str, Any] = _format.model_json_schema()  # type: ignore
-                schema_json: str = json.dumps(schema)
-                regex_str: str = outlines_core.fsm.json_schema.build_regex_from_schema(  # type: ignore
-                    schema_json
+                schema: dict[str, Any] = _format.model_json_schema()
+                grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
+                    schema, defaults={"whitespace_flexible": False}
                 )
-
-                from outlines.models.transformers import TransformerTokenizer
-                from outlines.processors.structured import RegexLogitsProcessor
-                from transformers import LogitsProcessorList  # type: ignore
-
+                logits_processor = _GuidanceLogitsProcessor(
+                    grammar, self._llguidance_tokenizer
+                )
                 format_kwargs["logits_processor"] = LogitsProcessorList(
-                    [
-                        RegexLogitsProcessor(
-                            regex_str, tokenizer=TransformerTokenizer(self._tokenizer)
-                        )
-                    ]
+                    [logits_processor]
                 )
 
             streaming_kwargs = {}
@@ -776,24 +834,15 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
 
             format_kwargs = {}
             if _format:
-                # outlines.generate.json always parses the resulting json into a python dict.
-                # We however want to keep it as a json string for later storing it in ModelOutputThunk
-                schema: dict[str, Any] = _format.model_json_schema()  # type: ignore
-                schema_json: str = json.dumps(schema)
-                regex_str: str = outlines_core.fsm.json_schema.build_regex_from_schema(  # type: ignore
-                    schema_json
+                schema: dict[str, Any] = _format.model_json_schema()
+                grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
+                    schema, defaults={"whitespace_flexible": False}
                 )
-
-                from outlines.models.transformers import TransformerTokenizer
-                from outlines.processors.structured import RegexLogitsProcessor
-                from transformers import LogitsProcessorList  # type: ignore
-
+                logits_processor = _GuidanceLogitsProcessor(
+                    grammar, self._llguidance_tokenizer
+                )
                 format_kwargs["logits_processor"] = LogitsProcessorList(
-                    [
-                        RegexLogitsProcessor(
-                            regex_str, tokenizer=TransformerTokenizer(self._tokenizer)
-                        )
-                    ]
+                    [logits_processor]
                 )
 
             streaming_kwargs = {}
@@ -1043,24 +1092,15 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
 
             format_kwargs = {}
             if format:
-                # outlines.generate.json always parses the resulting json into a python dict.
-                # We however want to keep it as a json string for later storing it in ModelOutputThunk
-                schema: dict[str, Any] = format.model_json_schema()  # type: ignore
-                schema_json: str = json.dumps(schema)
-                regex_str: str = outlines_core.fsm.json_schema.build_regex_from_schema(  # type: ignore
-                    schema_json
+                schema: dict[str, Any] = format.model_json_schema()
+                grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
+                    schema, defaults={"whitespace_flexible": False}
                 )
-
-                from outlines.models.transformers import TransformerTokenizer
-                from outlines.processors.structured import RegexLogitsProcessor
-                from transformers import LogitsProcessorList  # type: ignore
-
+                logits_processor = _GuidanceLogitsProcessor(
+                    grammar, self._llguidance_tokenizer
+                )
                 format_kwargs["logits_processor"] = LogitsProcessorList(
-                    [
-                        RegexLogitsProcessor(
-                            regex_str, tokenizer=TransformerTokenizer(self._tokenizer)
-                        )
-                    ]
+                    [logits_processor]
                 )
 
             outputs = await asyncio.to_thread(

conda/environment.yml
@@ -5,5 +5,3 @@ channels:
 dependencies:
   - python=3.12                 # note: at the time of writing, xformer (< vllm) has a broken wheel for 3.13. https://github.com/facebookresearch/xformers/issues/740#issuecomment-2753869337
   - uv
-variables:
-  VLLM_USE_V1: 0                 # need this to make outlines work

conda/install.sh
@@ -4,8 +4,7 @@ CONDA=""
 if which mamba > /dev/null
 then
     CONDA=$(which mamba)
-fi
-if which conda > /dev/null
+elif which conda > /dev/null
 then
     CONDA=$(which conda)
 fi
@@ -53,7 +52,7 @@ while getopts "yh" OPTNAME ; do
     esac
 done
 
-if $CONDA env list | grep -q mellea
+if ($CONDA activate mellea)
 then
     echo "An existing mellea environment was found."
     $CONDA env remove $CONDA_OPTIONS -n mellea

docs/kv_smash/kvcache.py
@@ -6,12 +6,14 @@
 # ///
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer
+from transformers.generation import GenerateDecoderOnlyOutput
 
 from mellea.backends.kv_block_helpers import DynamicCache, merge_dynamic_caches
 
 model_id = "ibm-granite/granite-4.0-tiny-preview"
 device = torch.device("mps")
-model = AutoModelForCausalLM.from_pretrained(model_id).to(device)
+model = AutoModelForCausalLM.from_pretrained(model_id)
+# model = model.to(device=device) # this part does not pass mypy; possible misconfiguration
 tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(model_id)
 
 
@@ -19,8 +21,8 @@ def cache(toks) -> DynamicCache:
     dc = DynamicCache()
     with torch.no_grad():
         rv = model(
-            toks["input_ids"].to(device),
-            attention_mask=toks["attention_mask"].to(device),
+            toks["input_ids"].to(model.device),
+            attention_mask=toks["attention_mask"].to(model.device),
             past_key_values=dc,
         ).past_key_values
     return rv
@@ -42,14 +44,17 @@ strs = ["this is a test", "this is another test"]
 merged_toks, merged_masks, merged_dcs = merge(strs)
 merged_dcs.crop(-1)
 
+# GenerateDecoderOnlyOutput | GenerateEncoderDecoderOutput | GenerateBeamDecoderOnlyOutput | GenerateBeamEncoderDecoderOutput | LongTensor
 result = model.generate(
-    merged_toks.to(device),
-    attention_mask=merged_masks.to(device),
+    merged_toks.to(model.device),
+    attention_mask=merged_masks.to(model.device),
     past_key_values=merged_dcs,
     use_cache=True,
     return_dict_in_generate=True,
     output_scores=True,
 )
+
+assert isinstance(result, GenerateDecoderOnlyOutput)
 
 result_decoded = tokenizer.decode(
     result.sequences[0, merged_toks.shape[1] :], skip_special_tokens=True

commit b9115d86b9cb7eae890d9b0d45299672c4963824

fix(hf): set whitespace_flexible=True in all grammar_from_json_schema calls
With whitespace_flexible=False (compact JSON), llguidance constructs a grammar
where the highest-probability token after opening an array '[' is immediately
']', silently collapsing structured output to empty arrays (e.g.
{"result":[]}) under greedy decoding.  No exception is raised and the caller
cannot distinguish a correct empty list from a grammar-induced collapse.

This bug was introduced in PR #288 as a mechanical outlines->llguidance port.
The llguidance library's own default for whitespace_flexible is True; the
original False was never intentional.

Fix all three grammar_from_json_schema call sites in LocalHFBackend:
- _generate_from_context_with_kv_cache
- _generate_from_context_standard
- _generate_from_raw

Regression tests added for all three call sites (mock-based, no real model).

Fixes #1510

Assisted-by: IBM Bob
Signed-off-by: Nigel Jones <jonesn@uk.ibm.com>

mellea/backends/huggingface.py
@@ -1031,8 +1031,14 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
             format_kwargs = {}
             if _format:
                 schema: dict[str, Any] = _format.model_json_schema()
+                # whitespace_flexible=True (natural, spaced JSON) is required.
+                # False (compact JSON) can put the model into a state where the
+                # highest-probability grammar-compatible token closes an array
+                # immediately, silently collapsing {"result": [...]} to {"result": []}.
+                # No exception is raised — the caller cannot distinguish a correct
+                # empty list from a collapse. See issue #1510 for full characterisation.
                 grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
-                    schema, defaults={"whitespace_flexible": False}
+                    schema, defaults={"whitespace_flexible": True}
                 )
                 logits_processor = _GuidanceLogitsProcessor(
                     grammar, self._llguidance_tokenizer
@@ -1236,8 +1242,14 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
             format_kwargs = {}
             if _format:
                 schema: dict[str, Any] = _format.model_json_schema()
+                # whitespace_flexible=True (natural, spaced JSON) is required.
+                # False (compact JSON) can put the model into a state where the
+                # highest-probability grammar-compatible token closes an array
+                # immediately, silently collapsing {"result": [...]} to {"result": []}.
+                # No exception is raised — the caller cannot distinguish a correct
+                # empty list from a collapse. See issue #1510 for full characterisation.
                 grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
-                    schema, defaults={"whitespace_flexible": False}
+                    schema, defaults={"whitespace_flexible": True}
                 )
                 logits_processor = _GuidanceLogitsProcessor(
                     grammar, self._llguidance_tokenizer
@@ -1701,8 +1713,14 @@ class LocalHFBackend(FormatterBackend, AdapterMixin):
         format_kwargs = {}
         if format:
             schema: dict[str, Any] = format.model_json_schema()
+            # whitespace_flexible=True (natural, spaced JSON) is required.
+            # False (compact JSON) can put the model into a state where the
+            # highest-probability grammar-compatible token closes an array
+            # immediately, silently collapsing {"result": [...]} to {"result": []}.
+            # No exception is raised — the caller cannot distinguish a correct
+            # empty list from a collapse. See issue #1510 for full characterisation.
             grammar: str = llguidance.LLMatcher.grammar_from_json_schema(
-                schema, defaults={"whitespace_flexible": False}
+                schema, defaults={"whitespace_flexible": True}
             )
             logits_processor = _GuidanceLogitsProcessor(
                 grammar, self._llguidance_tokenizer

test/backends/test_huggingface_unit.py
@@ -977,3 +977,167 @@ async def test_multimodal_blocks_in_intrinsic_ctx_raise_error(
         await LocalHFBackend._generate_from_intrinsic(
             backend, Intrinsic("answerability"), ctx, model_options={}
         )
+
+
+# ---------------------------------------------------------------------------
+# Regression tests for issue #1510: whitespace_flexible must be True
+# ---------------------------------------------------------------------------
+# llguidance's whitespace_flexible=False (compact JSON) forces greedy decoding
+# into states where the highest-probability grammar-compatible token closes an
+# array immediately, silently collapsing {"result": [...]} to {"result": []}.
+# All three grammar_from_json_schema call sites in LocalHFBackend must pass
+# whitespace_flexible=True. These tests assert that invariant via mock without
+# loading any real model.
+
+
+class _FakeSchema:
+    """Minimal Pydantic-compatible schema stub."""
+
+    @staticmethod
+    def model_json_schema() -> dict:
+        return {"type": "object", "properties": {"result": {"type": "array"}}}
+
+
+def _mock_chat_template_output() -> MagicMock:
+    """Return a mock that looks like a tokenizer output dict with a .to() method.
+
+    apply_chat_template returns a BatchEncoding (dict-like) which gets a .to(device)
+    call immediately after. Plain dicts don't have .to(), so the mock must.
+    """
+    ids = torch.zeros(1, 4, dtype=torch.long)
+    attn = torch.ones(1, 4, dtype=torch.long)
+    obj = MagicMock()
+    obj.__getitem__ = lambda s, k: ids if k == "input_ids" else attn
+    obj.to = lambda device: obj
+    obj["input_ids"] = ids
+    obj["attention_mask"] = attn
+    return obj
+
+
+@pytest.mark.asyncio
+async def test_whitespace_flexible_true_in_generate_from_context_standard():
+    """Regression (#1510): _generate_from_context_standard must call
+    grammar_from_json_schema with whitespace_flexible=True.
+
+    Without the fix, the call passes whitespace_flexible=False, which can cause
+    silent array collapse to [] under greedy decoding.
+    """
+    # _make_backend() patches llguidance during construction; re-patch just for
+    # the method call to intercept the grammar_from_json_schema invocation.
+    backend = _make_backend()
+    backend._tokenizer = MagicMock()
+    backend._tokenizer.apply_chat_template.return_value = _mock_chat_template_output()
+    backend._model = MagicMock()
+    backend._model.generate.return_value = torch.zeros(1, 5, dtype=torch.long)
+    ctx = ChatContext().add(Message("user", "list facts"))
+
+    captured: list[dict] = []
+
+    def _capture_grammar(schema, defaults=None):
+        captured.append(defaults or {})
+        return "stub-grammar"
+
+    with patch("mellea.backends.huggingface.llguidance") as mock_llg:
+        mock_llg.LLMatcher.grammar_from_json_schema.side_effect = _capture_grammar
+        mock_llg.LLMatcher.return_value = MagicMock()
+        mock_llg.torch.allocate_token_bitmask.return_value = MagicMock()
+        try:
+            await backend._generate_from_context_standard(
+                Instruction(description="test"),
+                ctx,
+                model_options={},
+                _format=_FakeSchema,
+            )
+        except Exception:
+            pass  # We only care that the grammar call was made correctly.
+
+    assert captured, "grammar_from_json_schema was never called"
+    for call_defaults in captured:
+        assert call_defaults.get("whitespace_flexible") is True, (
+            f"Expected whitespace_flexible=True, got {call_defaults!r} — "
+            "see issue #1510: False causes silent empty-array collapse"
+        )
+
+
+@pytest.mark.asyncio
+async def test_whitespace_flexible_true_in_generate_from_raw():
+    """Regression (#1510): _generate_from_raw must call grammar_from_json_schema
+    with whitespace_flexible=True.
+    """
+    backend = _make_backend()
+    # _generate_from_raw calls self._tokenizer(prompts, ...).to(device), so the
+    # mock tokenizer must be callable and return a .to()-able object.
+    tok_output = MagicMock()
+    tok_output.to = lambda device: tok_output
+    tok_output.__getitem__ = lambda s, k: torch.zeros(1, 4, dtype=torch.long)
+    backend._tokenizer = MagicMock(return_value=tok_output)
+    backend._model = MagicMock()
+    backend._model.generate.return_value = torch.zeros(1, 5, dtype=torch.long)
+    ctx = ChatContext().add(Message("user", "list facts"))
+
+    captured: list[dict] = []
+
+    def _capture_grammar(schema, defaults=None):
+        captured.append(defaults or {})
+        return "stub-grammar"
+
+    with patch("mellea.backends.huggingface.llguidance") as mock_llg:
+        mock_llg.LLMatcher.grammar_from_json_schema.side_effect = _capture_grammar
+        mock_llg.LLMatcher.return_value = MagicMock()
+        mock_llg.torch.allocate_token_bitmask.return_value = MagicMock()
+        try:
+            await backend._generate_from_raw(
+                [Instruction(description="test")],
+                ctx,
+                format=_FakeSchema,
+                model_options={},
+            )
+        except Exception:
+            pass
+
+    assert captured, "grammar_from_json_schema was never called"
+    for call_defaults in captured:
+        assert call_defaults.get("whitespace_flexible") is True, (
+            f"Expected whitespace_flexible=True, got {call_defaults!r} — "
+            "see issue #1510: False causes silent empty-array collapse"
+        )
+
+
+@pytest.mark.asyncio
+async def test_whitespace_flexible_true_in_generate_from_context_with_kv_cache():
+    """Regression (#1510): _generate_from_context_with_kv_cache must call
+    grammar_from_json_schema with whitespace_flexible=True.
+    """
+    backend = _make_backend()
+    backend._tokenizer = MagicMock()
+    backend._tokenizer.apply_chat_template.return_value = _mock_chat_template_output()
+    backend._model = MagicMock()
+    backend._model.generate.return_value = torch.zeros(1, 5, dtype=torch.long)
+    ctx = ChatContext().add(Message("user", "list facts"))
+
+    captured: list[dict] = []
+
+    def _capture_grammar(schema, defaults=None):
+        captured.append(defaults or {})
+        return "stub-grammar"
+
+    with patch("mellea.backends.huggingface.llguidance") as mock_llg:
+        mock_llg.LLMatcher.grammar_from_json_schema.side_effect = _capture_grammar
+        mock_llg.LLMatcher.return_value = MagicMock()
+        mock_llg.torch.allocate_token_bitmask.return_value = MagicMock()
+        try:
+            await backend._generate_from_context_with_kv_cache(
+                Instruction(description="test"),
+                ctx,
+                model_options={},
+                _format=_FakeSchema,
+            )
+        except Exception:
+            pass
+
+    assert captured, "grammar_from_json_schema was never called"
+    for call_defaults in captured:
+        assert call_defaults.get("whitespace_flexible") is True, (
+            f"Expected whitespace_flexible=True, got {call_defaults!r} — "
+            "see issue #1510: False causes silent empty-array collapse"
+        )