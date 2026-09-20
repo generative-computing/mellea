bc55fdf5744d3b17152eddd1f5a2bb44706fc19b
Vishal V <56761954+cptnm3@users.noreply.github.com>
2026-09-20 15:06:09 +0530

    fixup: Update mellea/stdlib/sampling/sofai.py
    Co-authored-by: Alex Bozarth <ajbozart@us.ibm.com>
    Signed-off-by: Vishal V <56761954+cptnm3@users.noreply.github.com>

--- mellea/stdlib/sampling/sofai.py
+++ mellea/stdlib/sampling/sofai.py
@@ -664,6 +664,7 @@ class SOFAISamplingStrategy(SamplingStrategy):
             next_action = deepcopy(action)
             next_context: Context = context
 
+            show_progress = show_progress and flog.getEffectiveLevel() <= MelleaLogger.INFO
             loop_iterator = (
                 tqdm.tqdm(range(effective_loop_budget), desc="S1 Solver")
                 if show_progress
